405ed514f52f03dae3f7a46085503ff5e5134fa7
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2026-07-29 14:27:43 -0600

    [FIX] account_caba_payment_date: bump module version task#32132

--- account_caba_payment_date/__manifest__.py
+++ account_caba_payment_date/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Account CABA Payment Date",
-    "version": "17.0.2.0.0",
+    "version": "17.0.2.1.0",
     "author": "Jarsa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-payment",
     "license": "LGPL-3",
